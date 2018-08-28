4546045abb8e41f3a1cf2421aab8cfb8060825f2
stdio2016 <randomfrank8@gmail.com>
2018-08-28 22:10:11 +0800

    MCTS!

--- fun/mcts.js
+++ fun/mcts.js
@@ -1,11 +1,77 @@
-function Mcts() {
+function Mcts(state, player) {
   this.parent = null;
   this.children = [];
-  this.state = null;
-  this.visited = 0;
+  this.state = state;
+  this.player = player;
+  this.move = null;
+  this.tried = 0;
   this.win = 0;
 }
 
+Mcts.prototype.UCT = function () {
+  var root = this;
+  while (root.parent) {
+    root = root.parent;
+  }
+  return +
+    this.win / (this.tried+1) +
+    Math.sqrt(2 * Math.log(root.tried) / (this.tried+1));
+};
+
+Mcts.prototype.select = function () {
+  if (this.children.length === 0) return this;
+  var i, best = this.children[0], bestuct = best.UCT();
+  for (i = 1; i < this.children.length; i++) {
+    var uct = this.children[i].UCT();
+    if (uct > bestuct) {
+      best = this.children[i];
+      bestuct = uct;
+    }
+  }
+  return best.select();
+};
+
 Mcts.prototype.expand = function () {
-  
+  var choice = getNextMoves(this.state, this.player);
+  var opponent = 3 - this.player;
+  if (choice.length === 0) return ;
+  for (var i = 0; i < choice.length; i++) {
+    var next = simulateAction(this.state, choice[i], this.player);
+    var node = new Mcts(next, opponent);
+    node.move = choice[i];
+    node.parent = this;
+    this.children.push(node);
+  }
+  var r = (Math.random() * choice.length) | 0;
+  return this.children[r];
+};
+
+Mcts.prototype.simulate = function () {
+  var player = this.player;
+  var state = cloneState(this.state);
+  while (1) {
+    var choice = getNextMoves(state, player);
+    if (choice.length === 0) {
+      // end game
+      return getWinLose(state, player);
+    }
+    // random move!
+    var r = (Math.random() * choice.length) | 0;
+    var action = choice[r];
+    doAction(state, action, player);
+    // next player
+    var opponent = 3 - player;
+    player = opponent;
+  }
+};
+
+Mcts.prototype.propagate = function (player) {
+  var node = this;
+  while (node) {
+    if (node.player === player) {
+      node.win++;
+    }
+    node.tried++;
+    node = node.parent;
+  }
 };
--- fun/reversi.js
+++ fun/reversi.js
@@ -1,5 +1,77 @@
 window.Int8Array = window.Int8Array || Array;
 
-function getNextMoves(state) {
-  
+
+function getNextMoves(state, player) {
+  var action = [];
+  for (var i = 0; i < 8; i++) {
+    for (var j = 0; j < 8; j++) {
+      if (state[i*8+j] === 0) {
+        action.push(i*8+j);
+      }
+    }
+  }
+  return action;
+}
+
+function cloneState(state) {
+  var r = new Int8Array(64);
+  r.set(state);
+  return r;
+}
+
+function simulateAction(state, action, player) {
+  var next = cloneState(state);
+  doAction(next, action, player);
+  return next;
+}
+
+function doAction(state, action, player) {
+  state[action] = player;
+  reversiSim(action>>3, action&7, player, state);
+}
+
+function getWinLose(state, player) {
+  var cntBlack = 0, cntWhite = 0;
+  for (var i = 0; i < 8; i++) {
+    for (var j = 0; j < 8; j++) {
+      if (state[i*8+j] === FIRST_PLAYER) {
+        cntBlack++;
+      }
+      if (state[i*8+j] === SECOND_PLAYER) {
+        cntWhite++;
+      }
+    }
+  }
+  if (cntBlack > cntWhite) return FIRST_PLAYER;
+  if (cntWhite > cntBlack) return SECOND_PLAYER;
+  return 0;
+}
+
+function reversiSimHelper(i, j, who, board, di, dj) {
+  var opponent = 3 - who;
+  var y = i, x = j;
+  y += di;
+  x += dj;
+  while (y >= 0 && y < 8 && x >= 0 && x < 8 && board[y*8+x] === opponent) {
+    y += di;
+    x += dj;
+  }
+  if (y >= 0 && y < 8 && x >= 0 && x < 8 && board[y*8+x] === who) {
+    while (y !== i || x !== j) {
+      board[y*8+x] = who;
+      y -= di;
+      x -= dj;
+    }
+  }
+}
+
+function reversiSim(i, j, who, board) {
+  reversiSimHelper(i, j, who, board, 1, 1);
+  reversiSimHelper(i, j, who, board, 1, -1);
+  reversiSimHelper(i, j, who, board, -1, 1);
+  reversiSimHelper(i, j, who, board, -1, -1);
+  reversiSimHelper(i, j, who, board, 1, 0);
+  reversiSimHelper(i, j, who, board, -1, 0);
+  reversiSimHelper(i, j, who, board, 0, 1);
+  reversiSimHelper(i, j, who, board, 0, -1);
 }
